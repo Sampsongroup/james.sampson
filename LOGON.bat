e211dc5940641ed5fe47c252cb09d910d407c4ed
jsamps01 <84640024+jsamps01@users.noreply.github.com>
2021-11-07 10:54:57 -0800

    Revert "Create LOGON.bat"
    This reverts commit 2cdf1d816f8e3dd0c0e41cc0d7c0834322c8e9eb.

--- LOGON.bat
+++
@@ -1,6 +0,0 @@
-@Clear
-net use H: \\SAMPSONGROUP\AMY_PC user:amy.sampson@sampsongroup.net /Persistent:yes
-net use H: \\SAMPSONGROUP\JAMES_PC user:james.sampson@sampsongroup.net /Persistent:yes
-net use I: \\SAMPSONGROUP\AMY_PC user:james.sampson@sampsongroup.net /Persistent:yes
-net use O: \\SAMPSONGROUP\SOFTWARE /Persistent:yes
-END
